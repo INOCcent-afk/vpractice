d3a689ffb9ad56c5561d033feefafc871f4e4655
Dave Inoc <michael828inoc@gmail.com>
2021-06-14 16:19:19 +0800

    init commit

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button @click="click()" :style="{ background: color }" class="btn">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    text: String,
+    color: String,
+  },
+  methods: {
+    click() {
+      console.log("click");
+    },
+  },
+};
+</script>
